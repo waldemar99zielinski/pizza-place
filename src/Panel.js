5ba1a33f86c8d3d8403275b4fdffafa7f4d57f4b
Waldemar <waldek99zielinski@gmail.com>
2021-01-14 22:43:10 +0100

    pizza popup init

--- src/Panel.js
+++ src/Panel.js
@@ -1,19 +1,72 @@
 import React from "react";
 import "./Panel.css";
-import PizzaPanel from "./pizzaPanel/PizzaPanel";
+import PizzaOverView from "./pizzaPanel/PizzaOverView";
+import PizzaPopUp from "./pizzaPanel/PizzaPopUp";
+const data = [
+  {
+    id: 1,
+    name: "Wiejska",
+    rating: 4.3,
+    price: 12,
+    photo: "https://assets.puzzlefactory.pl/puzzle/238/423/original.jpg",
+  },
+  {
+    id: 2,
+    name: "owoce morza",
+    rating: 3.0,
+    price: 25,
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Pizza_with_tomatoes.jpg/1200px-Pizza_with_tomatoes.jpg",
+  },
+  {
+    id: 3,
+    name: "Wiejska",
+    rating: 4.3,
+    price: 12,
+    photo:
+      "https://www.mojegotowanie.pl/uploads/media/recipe/0001/100/pizza-z-kurczakiem.jpeg",
+  },
+
+  {
+    id: 1,
+    name: "Wiejska",
+    rating: 4.3,
+    price: 12,
+    photo: "https://assets.puzzlefactory.pl/puzzle/238/423/original.jpg",
+  },
+  {
+    id: 2,
+    name: "owoce morza",
+    rating: 3.0,
+    price: 25,
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Pizza_with_tomatoes.jpg/1200px-Pizza_with_tomatoes.jpg",
+  },
+  {
+    id: 3,
+    name: "Wiejska",
+    rating: 4.3,
+    price: 12,
+    photo:
+      "https://www.mojegotowanie.pl/uploads/media/recipe/0001/100/pizza-z-kurczakiem.jpeg",
+  },
+  {
+    id: 4,
+    name: "Wiejska",
+    rating: 4.3,
+    price: 12,
+    photo: "https://assets.puzzlefactory.pl/puzzle/238/423/original.jpg",
+  },
+];
+
 const Panel = () => {
+  console.log();
   return (
-    <div className="panel-container">
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
-      <PizzaPanel />
+    <div>
+      <div className="panel-container">
+        <PizzaOverView data={data} />
+        <PizzaPopUp />
+      </div>
     </div>
   );
 };
--- src/pizzaPanel/PizzaPanel.js
+++ src/pizzaPanel/PizzaPanel.js
@@ -1,25 +1,21 @@
 import React from "react";
 import "./PizzaPanel.css";
 import { AiFillStar } from "react-icons/ai";
-const PizzaPanel = () => {
+const PizzaPanel = (props) => {
   return (
     <div className="pizza-container" onClick={() => console.log("clicked")}>
-      <div>
-        <img
-          src="https://assets.puzzlefactory.pl/puzzle/238/423/original.jpg"
-          alt="pizza"
-          className="pizza-photo"
-        />
+      <div className="pizza-photo-container">
+        <img src={props.photo} alt="pizza" className="pizza-photo" />
       </div>
 
-      <h2>TITLE</h2>
+      <h2>{props.name.toUpperCase()}</h2>
       <div className="pizza-info-container">
         <div className="pizza-rating">
-          <h3>4.3</h3>
+          <h3>{props.rating}</h3>
           <AiFillStar />
         </div>
         <div className="pizza-prize">
-          <h3>12</h3>
+          <h3>{props.price}</h3>
           <h3>zl</h3>
         </div>
       </div>
