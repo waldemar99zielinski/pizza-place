ca257eec3dc4ab91610e401301f2f7c4cd6fbdf2
Waldemar <waldek99zielinski@gmail.com>
2021-04-01 14:56:31 +0200

    orderFinalization: radio input styling

--- src/components/orderFinalization/OrderInfoForm.js
+++ src/components/orderFinalization/OrderInfoForm.js
@@ -2,30 +2,54 @@ import React from "react";
 
 const OrderInfoForm = () => {
   return (
-    <div className="order-finalization-order-info-container">
-      <form>
-        <label>Delivery</label>
-        <div>
-          <input type="radio" id="male" name="gender" value="male" />
-          <label for="male">YES</label>
+    <form className="order-finalization-order-info">
+      <div className="order-finalization-radio-container">
+        <label className="order-finalization-radio-title">DELIVERY</label>
+        <div className="order-finalization-radio-group">
+          <input
+            className="order-finalization-radio-input"
+            type="radio"
+            id="yes"
+            name="delivery"
+          />
+          <label className="order-finalization-radio-label" for="yes">
+            YES
+          </label>
+          <input
+            className="order-finalization-radio-input"
+            type="radio"
+            id="no"
+            name="delivery"
+          />
+          <label className="order-finalization-radio-label" for="no">
+            NO
+          </label>
         </div>
-        <div>
-          <input type="radio" id="female" name="gender" value="female" />
-          <label for="female">NO</label>
+      </div>
+      <div className="order-finalization-radio-container">
+        <label className="order-finalization-radio-title">payment</label>
+        <div className="order-finalization-radio-group">
+          <input
+            className="order-finalization-radio-input"
+            type="radio"
+            id="money"
+            name="payment"
+          />
+          <label className="order-finalization-radio-label" for="money">
+            READY MONEY
+          </label>
+          <input
+            className="order-finalization-radio-input"
+            type="radio"
+            id="card"
+            name="payment"
+          />
+          <label className="order-finalization-radio-label" for="card">
+            CARD
+          </label>
         </div>
-      </form>
-      <form>
-        <label>Payment</label>
-        <div>
-          <input type="radio" id="male" name="gender" value="male" />
-          <label for="male">READY MONEY</label>
-        </div>
-        <div>
-          <input type="radio" id="female" name="gender" value="female" />
-          <label for="female">CARD</label>
-        </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
